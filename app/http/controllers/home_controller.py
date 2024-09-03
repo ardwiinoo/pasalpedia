b965185fa99abecb827533e7c127c6b37ec779e1
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-09-03 13:15:35 +0700

    add dummy data, pass data to views

--- app/http/controllers/home_controller.py
+++ app/http/controllers/home_controller.py
@@ -2,4 +2,14 @@ from flask import render_template
 
 class HomeController:
     def home():
-        return render_template('dashboard/home/index.html')
+
+        # dummy
+        items = [
+            {"data_applied": "Data 1", "status": "Active"},
+            {"data_applied": "Data 2", "status": "Inactive"},
+            {"data_applied": "Data 3", "status": "Pending"},
+            {"data_applied": "Data 4", "status": "Completed"},
+            {"data_applied": "Data 5", "status": "Reviewed"},
+        ]
+
+        return render_template('dashboard/home/index.html', items=items)
